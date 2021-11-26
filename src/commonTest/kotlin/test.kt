9dcf369dda60f4f7fef235b378eefbb782f71615
Alex5041 <kononal@gmail.com>
2021-11-26 14:46:23 +0300

    Deleted test

--- src/commonTest/kotlin/test.kt
+++ src/commonTest/kotlin/test.kt
@@ -8,19 +8,19 @@ import com.soywiz.korma.geom.*
 import kotlin.test.*
 
 class MyTest : ViewsForTesting() {
-    @Test
-    fun test() = viewsTest {
-        val log = arrayListOf<String>()
-        val rect = solidRect(100, 100, Colors.RED)
-        rect.onClick {
-            log += "clicked"
-        }
-        assertEquals(1, views.stage.numChildren)
-        rect.simulateClick()
-        assertEquals(true, rect.isVisibleToUser())
-        tween(rect::x[-102], time = 10.seconds)
-        assertEquals(Rectangle(x=-102, y=0, width=100, height=100), rect.globalBounds)
-        assertEquals(false, rect.isVisibleToUser())
-        assertEquals(listOf("clicked"), log)
-    }
+//    @Test
+//    fun test() = viewsTest {
+//        val log = arrayListOf<String>()
+//        val rect = solidRect(100, 100, Colors.RED)
+//        rect.onClick {
+//            log += "clicked"
+//        }
+//        assertEquals(1, views.stage.numChildren)
+//        rect.simulateClick()
+//        assertEquals(true, rect.isVisibleToUser())
+//        tween(rect::x[-102], time = 10.seconds)
+//        assertEquals(Rectangle(x=-102, y=0, width=100, height=100), rect.globalBounds)
+//        assertEquals(false, rect.isVisibleToUser())
+//        assertEquals(listOf("clicked"), log)
+//    }
 }
